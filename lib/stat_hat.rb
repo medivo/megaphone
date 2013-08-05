71d656ddca728a2e771e9fe92bba5e472dd132f6
Alex Ungur <alexaandru@gmail.com>
2013-08-05 15:17:45 +0300

    Made Megaphone::StatHat more generic

--- lib/stat_hat.rb
+++ lib/stat_hat.rb
@@ -1,8 +1,13 @@
 module Megaphone
 
   class StatHat
-    def post_count(code)
-      stat = StatHat::API.ez_post_count("tickets", code, 1)
+    def post_count(code, stat_name = 'tickets', count = 1, timestamp = nil)
+      stat = StatHat::API.ez_post_count(stat_name, code, count, timestamp)
+      puts "StatHat says: #{stat.status} #{stat.msg}"
+    end
+
+    def post_value(code, stat_name = 'tickets', value = 0, timestamp = nil)
+      stat = StatHat::API.ez_post_value(stat_name, code, value, timestamp)
       puts "StatHat says: #{stat.status} #{stat.msg}"
     end
   end
