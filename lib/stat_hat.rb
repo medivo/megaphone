a9a1161adc99e13938d195cec9de8a47e52f6433
Alex Ungur <alexaandru@gmail.com>
2013-08-05 17:40:51 +0300

    Ensured that StatHat status interpretation will not break on older (newer?) versions

--- lib/stat_hat.rb
+++ lib/stat_hat.rb
@@ -3,12 +3,16 @@ module Megaphone
   class StatHat
     def post_count(code, stat_name = 'tickets', count = 1, timestamp = nil)
       stat = ::StatHat::API.ez_post_count(stat_name, code, count, timestamp)
-      puts "StatHat says: #{stat.status} #{stat.msg}"
+      puts "StatHat says: #{interpret_status stat}"
     end
 
     def post_value(code, stat_name = 'tickets', value = 0, timestamp = nil)
       stat = ::StatHat::API.ez_post_value(stat_name, code, value, timestamp)
-      puts "StatHat says: #{stat.status} #{stat.msg}"
+      puts "StatHat says: #{interpret_status stat}"
+    end
+
+    def interpret_status(stat)
+      stat.respond_to?(:status) ? "#{stat.status} #{stat.msg}" : (stat && 'success' || 'failure')
     end
   end
 
