d041042ada2f1183dbe48c56df7844f02aebd1a4
Alex Ungur <alexaandru@gmail.com>
2013-08-05 17:28:43 +0300

    Fixed a constant lookup issue related to StatHat

--- lib/stat_hat.rb
+++ lib/stat_hat.rb
@@ -2,12 +2,12 @@ module Megaphone
 
   class StatHat
     def post_count(code, stat_name = 'tickets', count = 1, timestamp = nil)
-      stat = StatHat::API.ez_post_count(stat_name, code, count, timestamp)
+      stat = ::StatHat::API.ez_post_count(stat_name, code, count, timestamp)
       puts "StatHat says: #{stat.status} #{stat.msg}"
     end
 
     def post_value(code, stat_name = 'tickets', value = 0, timestamp = nil)
-      stat = StatHat::API.ez_post_value(stat_name, code, value, timestamp)
+      stat = ::StatHat::API.ez_post_value(stat_name, code, value, timestamp)
       puts "StatHat says: #{stat.status} #{stat.msg}"
     end
   end
